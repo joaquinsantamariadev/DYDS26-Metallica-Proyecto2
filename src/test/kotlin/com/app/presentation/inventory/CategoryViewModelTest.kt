c1935f6111a7270c8dac0aa68540b02a9ff04fc0
Martin Dupuy <dupuymartin7@gmail.com>
2026-06-20 09:44:17 -0300

    docs: complete Stage 2

--- src/test/kotlin/com/app/presentation/inventory/CategoryViewModelTest.kt
+++ src/test/kotlin/com/app/presentation/inventory/CategoryViewModelTest.kt
@@ -2,6 +2,7 @@ package com.app.presentation.inventory
 
 import com.app.data.FakeCategoryRepository
 import com.app.domain.entity.Category
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -15,6 +16,7 @@ class CategoryViewModelTest {
     @Test
     fun testAddCategory() = runBlocking {
         viewModel.addCategory("Lácteos")
+        delay(100)
         assertTrue(repo.insertCalled)
         assertEquals(1, repo.categoriesList.size)
         assertEquals("Lácteos", repo.categoriesList[0].name)
@@ -24,6 +26,7 @@ class CategoryViewModelTest {
     fun testDeleteCategory() = runBlocking {
         repo.categoriesList.add(Category(id = 1, name = "Lácteos"))
         viewModel.deleteCategory(1)
+        delay(100)
         assertTrue(repo.deleteCalled)
         assertTrue(repo.categoriesList.isEmpty())
     }
@@ -32,6 +35,7 @@ class CategoryViewModelTest {
     fun testUpdateCategory() = runBlocking {
         repo.categoriesList.add(Category(id = 1, name = "Lácteos"))
         viewModel.updateCategory(Category(id = 1, name = "Bebidas"))
+        delay(100)
         assertTrue(repo.updateCalled)
         assertEquals("Bebidas", repo.categoriesList[0].name)
     }
